cce6eaf3bfa833c08f83215852afc03102289aa5
Kunmi Williams <kumswilliams@gmail.com>
2022-11-23 18:43:08 +0000

    greeting fixed

--- src/pages/Newtab/components/greeting.jsx
+++ src/pages/Newtab/components/greeting.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { Text, Box } from '@chakra-ui/react';
+import { Text, Flex } from '@chakra-ui/react';
 
 export const Greeting = () => {
   // returns the greeting for the approriate time of day
   // returns the users name... which is taken from Google login otherwise just returns there.
 
-  // const user = chrome.identity.getProfileUserInfo((userInfo) => {
-  //   console.log(userInfo);
-  // });
-  // console.log('userInfo', user);
+  const [email, setEmail] = React.useState('');
+  chrome.identity.getProfileUserInfo({ accountStatus: 'ANY' }, function (info) {
+    if (info.email) {
+      setEmail(info.email);
+    }
+  });
+  console.log(email.split('@', 1));
 
   return (
-    <Box>
-      <Text> Hello,</Text>
-      <Text> there</Text>
-    </Box>
+    <Flex w="100%" justify="space-between">
+      <Flex>
+        {email ? (
+          <Text> Hello, {email.split('@', 1)}</Text>
+        ) : (
+          <Text> Hello there</Text>
+        )}
+      </Flex>
+      <Flex>Dark mode icon</Flex>
+    </Flex>
   );
 };
