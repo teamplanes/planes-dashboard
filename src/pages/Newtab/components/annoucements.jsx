50bfca18e8b7dcebf7fea0d4815920cecbbbbdf2
Jonathan Englund <jonathan@planes.agency>
2022-11-30 16:49:19 +0000

    chore: fix chakra styling

--- src/pages/Newtab/components/annoucements.jsx
+++ src/pages/Newtab/components/annoucements.jsx
@@ -1,5 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Text, Image, Stack, Heading, Card, CardBody } from '@chakra-ui/react';
+import {
+  Text,
+  Image,
+  Stack,
+  Heading,
+  Card,
+  CardBody,
+  Divider,
+  Flex,
+  Button,
+  Link,
+} from '@chakra-ui/react';
 import { getSlackFetch } from '../utils/getSlackFetch';
 
 const getMessages = async () => {
@@ -34,14 +45,28 @@ const getUser = async (userId) => {
   }
 };
 
+const getPermaLink = async (message) => {
+  try {
+    const response = await getSlackFetch(
+      `/chat.getPermalink?channel=C04CX09AFFS&message_ts=${message.ts}`
+    );
+    return response.permaLink;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const Announcements = () => {
   // WebClient instantiates a client that can call API methods
   // When using Bolt, you can use either `app.client` or the `client` passed to listeners.
   const [message, setMessage] = useState('');
   const [user, setUser] = useState('');
+  const [permaLink, setPermalink] = useState('');
   const test = async () => {
     const message = await getMessages();
     const user = await getUser(message.user);
+    const link = await getPermaLink(message);
+    setPermalink(link);
     setUser(user);
     setMessage(message.text);
   };
@@ -55,26 +80,42 @@ export const Announcements = () => {
       direction="row"
       overflow="hidden"
       variant="outline"
-      bg="#42C7FF"
-      w="500px"
+      bg="#1434A4"
+      w="50vw"
       h="300px"
-      ml="20%"
+      mx="auto"
       borderRadius={8}
     >
-      <Image
-        objectFit="cover"
-        maxW={{ base: '100%', sm: '200px' }}
-        src={user.image_192}
-        alt="User Avatar"
-      />
+      <Flex flexDir="column" w="100%">
+        <Flex flexDir="row" p={8} pb={0}>
+          <Image
+            objectFit="cover"
+            w="100px"
+            h="100px"
+            src={user.image_192}
+            alt="User Avatar"
+            borderRadius="50%"
+          />
 
-      <Stack>
-        <CardBody>
-          <Heading size="md">{user.first_name + ' ' + user.last_name}</Heading>
-
-          <Text py="2">{message}</Text>
-        </CardBody>
-      </Stack>
+          <Stack ml={2} w="100%" color="white">
+            <CardBody>
+              <Heading size="md" mt={0}>
+                {user.first_name + ' ' + user.last_name}
+              </Heading>
+              <Heading as="h3" size="sm">
+                {user.title}
+              </Heading>
+              <Text py="2" fontSize="24px">
+                {message}
+              </Text>
+              <Button colorScheme="blue" mb={2}>
+                View in Slack
+              </Button>
+              <Divider border="1px solid white" orientation="horizontal" />
+            </CardBody>
+          </Stack>
+        </Flex>
+      </Flex>
     </Card>
   );
 };
--- src/pages/Newtab/index.jsx
+++ src/pages/Newtab/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import Newtab from './Newtab';
 import './index.css';
 
-createRoot(window.document.querySelector('#app-container')).render(<Newtab />);
+createRoot(window.document.querySelector('#app-container')).render(
+  <ChakraProvider>
+    <Newtab />
+  </ChakraProvider>
+);
