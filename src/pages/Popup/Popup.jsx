4fac41bfd6b3350e08d9ae3d1bda6e747f6b467b
Casey Hoepner <36767936+caseyhoepner@users.noreply.github.com>
2022-12-05 11:17:51 +0000

    Add popup

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import './Popup.css';
-import { Text, Box, Button } from '@chakra-ui/react';
+import { Text, Box, Image } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../Newtab/components/theme';
+import planeImage from '../../assets/img/plane.png';
+import planesLogo from '../../assets/img/planes_logo.svg';
 
 const Popup = () => {
   return (
-    <Box>
-      <header>
-        <Text fontSize="40px" color="white">
-          PLANES
+    <ChakraProvider theme={theme}>
+      <Box
+        bg="cream"
+        w="300px"
+        h="400px"
+        p="40px"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        gap="30px"
+      >
+        <Image w="112px" src={planesLogo} alt="planes' logo" />
+        <Image w="100%" src={planeImage} alt="plane" />
+        <Text
+          fontWeight={500}
+          fontSize={15}
+          lineHeight="20px"
+          letterSpacing="-0.02em"
+          align="center"
+        >
+          Open a new tab to view the dashboard
         </Text>
-        {/* <a href="" target="_blank" rel="noreferrer"> */}
-          <Button>Go</Button>
-        {/* </a> */}
-      </header>
-    </Box>
+      </Box>
+    </ChakraProvider>
   );
 };
 
